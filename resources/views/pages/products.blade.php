e705bada4a189acc13d0a9d357e2cc1ba88f0d55
SameerLeopard <sameer.qureshi@leopardscourier.com>
2026-09-08 18:58:18 +0500

    paging

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -58,7 +58,7 @@
                     ? (request('search') ? route('products', ['search' => request('search')]) : route('products'))
                     : route('products', array_filter(['category' => $cat, 'search' => request('search')]));
             @endphp
-            <a href="{{ $catUrl }}" class="btn-filter {{ $isActive ? 'active' : '' }}">
+            <a href="{{ $catUrl }}" class="btn-filter {{ $isActive ? 'active' : '' }} text-decoration-none">
                 {{ $cat }}
             </a>
             @endforeach
@@ -145,7 +145,7 @@
         @endif
         @endif
 
-        
+
     </div>
 </section>
 
